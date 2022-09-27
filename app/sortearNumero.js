db1cff656cc7c36bafabe9549bc9790137ebda57
Otavio Magalhães <otaviomag1@outlook.com>
2022-09-27 19:51:10 -0300

    create

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 0;
+const maiorValor = 100;
+const numeroSecreto = geraNumero();
+
+function geraNumero(){
+    return parseInt(Math.random() *  (menorValor - maiorValor - 1)  + (maiorValor + 1));
+}
+
+console.log(`Numero Secreto:  ${numeroSecreto}`);
+
+
+const elementoMenorValor = document.getElementById("menor__valor");
+const elementoMaiorValor = document.getElementById("maior__valor");
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
